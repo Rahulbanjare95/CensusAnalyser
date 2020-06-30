f81f659623b055ddb7299e40d6d97e0fd7855d6a
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-30 07:56:01 +0530

    Added: Test case for wrong header to throw custom exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -6,13 +6,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.stream.StreamSupport;
+
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INDIA_CENSUS_CSV_FILE_PATH_WRONG_TYPE = "./src/test/resources/IndiaStateCensusData.txt";
-    private static final String INDIA_CENSUS_FILE_WRONG_DELIMITER="./src/test/resources/WrongDelimiterStateCensus.csv";
-
+    private static final String INDIA_CENSUS_FILE_WRONG_DELIMITER = "./src/test/resources/WrongDelimiterStateCensus.csv";
+    private static final String INDIA_CENSUS_FILE_WRONG_HEADER = "./src/test/resources/WrongHeader.csv";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -46,15 +48,27 @@ public class CensusAnalyserTest {
     }
 
     @Test
-    public void givenIndianCensusWith_WrongDelimiter_ShouldThrowException() throws CensusAnalyserException {
+    public void givenIndianCensusWith_WrongDelimiter_ShouldThrowException()  {
         try{
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_FILE_WRONG_DELIMITER);
         }catch (CensusAnalyserException e){
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMITER,e.type);
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMITER_HEADER,e.type);
         }
     }
+    @Test
+    public void givenIndianCensusWith_WrongHeader_ShouldThrowException() {
+        try{
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_FILE_WRONG_HEADER);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMITER_HEADER,e.type);
+        }
+    }
+
 
 }
