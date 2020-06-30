5404c0f896b15c3e5b87a2f0555a396518f60362
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-30 18:17:20 +0530

    Modified: IndiaStateCodeCSV to read csv correctly

--- src/main/java/censusanalyser/model/IndiaStateCodeCSV.java
+++ src/main/java/censusanalyser/model/IndiaStateCodeCSV.java
@@ -5,11 +5,12 @@ public class IndiaStateCodeCSV {
     @CsvBindByName(column = "SrNo", required = true)
     public String SrNo;
     @CsvBindByName(column = "State Name", required = true)
-    public int StateName;
+    public String StateName;
     @CsvBindByName(column = "TIN", required = true)
     public int TIN;
     @CsvBindByName(column = "StateCode", required = true)
-    public int StateCode;
+    public String StateCode;
+
     @Override
     public String toString() {
         return "IndiaStateCodeCSV{" +
