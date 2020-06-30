bab4767649cd97db29a5348b5fcd451d81c28a38
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-30 20:26:05 +0530

    Added: Deligation to OpenCsvBuilder for separately handling Iterator

---
+++ src/main/java/censusanalyser/service/OpenCsvBuilder.java
@@ -0,0 +1,24 @@
+package censusanalyser.service;
+
+import censusanalyser.exceptions.CensusAnalyserException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCsvBuilder {
+
+    public <E>Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<E>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return  csvToBean.iterator();
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
+}
+
