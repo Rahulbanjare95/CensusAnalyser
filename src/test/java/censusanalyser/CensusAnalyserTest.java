79d9f34b2fe7be4f6e36ec0de8de214e85d769ad
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-30 08:51:17 +0530

    Added: Test case to check for the number of State code entries

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -15,6 +15,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH_WRONG_TYPE = "./src/test/resources/IndiaStateCensusData.txt";
     private static final String INDIA_CENSUS_FILE_WRONG_DELIMITER = "./src/test/resources/WrongDelimiterStateCensus.csv";
     private static final String INDIA_CENSUS_FILE_WRONG_HEADER = "./src/test/resources/WrongHeader.csv";
+    private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -70,5 +71,15 @@ public class CensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenIndianStateCSV_ShouldReturnExactCount() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfStateCode = censusAnalyser.loadIndiaStateData(INDIA_STATE_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfStateCode);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
 
+    }
 }
