77149242276498dd73c00a4d32c7b8d452a122d4
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-30 08:49:14 +0530

    Added: IndianStateCodeCSV to add State details

---
+++ src/main/java/censusanalyser/model/IndiaStateCodeCSV.java
@@ -0,0 +1,27 @@
+package censusanalyser.model;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCodeCSV {
+    @CsvBindByName(column = "SrNo", required = true)
+    public String SrNo;
+
+    @CsvBindByName(column = "State Name", required = true)
+    public int StateName;
+
+    @CsvBindByName(column = "TIN", required = true)
+    public int TIN;
+
+    @CsvBindByName(column = "StateCode", required = true)
+    public int StateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaStateCodeCSV{" +
+                "SrNo='" + SrNo + '\'' +
+                ", StateName=" + StateName +
+                ", TIN=" + TIN +
+                ", StateCode=" + StateCode +
+                '}';
+    }
+}
