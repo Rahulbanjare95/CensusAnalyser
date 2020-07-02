d3888d91677decfcf0506df5d249a3dad1de9544
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-03 05:26:47 +0530

    Added: Method to sort US census data on water area

--- src/main/java/censusanalyser/service/CensusAnalyser.java
+++ src/main/java/censusanalyser/service/CensusAnalyser.java
@@ -203,6 +203,16 @@ public class CensusAnalyser {
         String sortedPopulation = new Gson().toJson(usCensusCSVList);
         return sortedPopulation;
     }
+
+    public String getWaterAreaWiseSortedUSCensusData() throws CensusAnalyserException {
+        if (usCensusCSVList == null || usCensusCSVList.size() == 0) {
+            throw new CensusAnalyserException("No census data found", CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
+        }
+        Comparator<USCensusCSV> censusCSVComparator = Comparator.comparing(census -> census.waterArea);
+        this.sortDescending(censusCSVComparator, usCensusCSVList);
+        String sortedPopulation = new Gson().toJson(usCensusCSVList);
+        return sortedPopulation;
+    }
 }
 
 
